ad7fd6ceab99eec022ee2a57e8c894003739cedb
Sujal Bhavsar <sujalofficial011@gmail.com>
2025-04-06 12:55:53 +0530

    added more jobRoutes  - getJobByTitle  - getJobBySkills

--- src/controlllers/jobController.js
+++ src/controlllers/jobController.js
@@ -19,37 +19,49 @@ export const createJob = async (req, res) => {
         };
 
         const job = await jobService.createJob(jobData);
-        res.status(201).json(job);
+        return res.status(201).json(job);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
 
 export const getAllJobs = async (req, res) => {
     try {
         const jobs = await jobService.getAllJobs();
-        res.json(jobs);
+        return res.json(jobs);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
+
+export const getJobsByTitle = async (req, res) => {
+    try {
+        const title = req.query.title;
+        const job = await jobService.getJobsByTitle(title);
+        if (!job) return res.status(404).json({ message: "Job not found" });
+        return res.json(job);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
 
 export const getJobsBySkills = async (req, res) => {
     try {
         const { skills } = req.body;
         const jobs = await jobService.getJobsBySkills(skills);
-        res.json(jobs);
+        if(jobs.length === 0) return res.status(404).json({ message: "No jobs found with the specified skills" });
+        return res.json(jobs);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
 
 export const getEmployerJobs = async (req, res) => {
     try {
         const jobs = await jobService.getJobsByEmployer(req.user.id);
-        res.json(jobs);
+        return res.json(jobs);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
 
@@ -57,8 +69,8 @@ export const deleteJob = async (req, res) => {
     try {
         const job = await jobService.deleteJob(req.params.id, req.user.id);
         if (!job) return res.status(403).json({ message: "Not authorized or job not found" });
-        res.json({ message: "Job deleted successfully" });
+        return res.json({ message: "Job deleted successfully" });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post("/", protect, jobController.createJob);
 router.get("/", protect,jobController.getAllJobs);
 router.get("/employer/my-jobs", protect, jobController.getEmployerJobs);
+router.get("/search", protect, jobController.getJobsByTitle);
+router.get("/skills", protect, jobController.getJobsBySkills);
 router.delete("/:id", protect, jobController.deleteJob);
 
 export default router;
